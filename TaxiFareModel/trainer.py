d0016252bfd30b2ac91d2256e3dcf3da5e4bb186
Clara Martiny <clara.msy@gmail.com>
2020-11-24 16:25:24 +0100

    updated trainer w mlflow

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -5,6 +5,14 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
+import mlflow
+from  mlflow.tracking import MlflowClient
+
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+myname = "clara"
+EXPERIMENT_NAME = f"TaxifareModel_{myname}"
 
 class Trainer():
     def __init__(self, X, y):
@@ -15,6 +23,8 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+
+        self.mlflow_log_param(param, value)
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -36,6 +46,35 @@ class Trainer():
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         return compute_rmse(y_pred, y_test)
+    
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+    
+    def train(self):
+
+        for model in ["linear", "Randomforest"]:
+            self.mlflow_create_run()
+            self.mlflow_log_metric("rmse", 4.5)
+            self.mlflow_log_param("model", model)
 
 
 if __name__ == "__main__":
