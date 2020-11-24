9cd782aafc09445ea511930434175c1bd675e2ad
Clara Martiny <clara.msy@gmail.com>
2020-11-24 15:41:15 +0100

    first commit

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,4 +1,10 @@
 # imports
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.preprocessing import StandardScaler, RobustScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from TaxiFareModel.utils import compute_rmse
 
 class Trainer():
     def __init__(self, X, y):
@@ -12,15 +18,24 @@ class Trainer():
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
-        pass
+        pipe_distance = make_pipeline(DistanceTransformer(),StandardScaler())
+        pipe_time = make_pipeline(TimeFeaturesEncoder(time_column='pickup_datetime'),OneHotEncoder())
+        dist_cols = ['pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude']
+        time_cols = ['pickup_datetime']
+        feat_eng_bloc = ColumnTransformer([('distance',pipe_distance,dist_cols),
+                                    ('time',pipe_time,time_cols)])
+        self.pipeline = Pipeline(steps=[('feat_eng_bloc',feat_eng_bloc),
+                        ('regressor',LinearRegression())])
 
     def run(self):
         """set and train the pipeline"""
-        pass
+        self.set_pipeline()
+        self.pipeline.fit(self.X,self.y)
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
-        pass
+        y_pred = self.pipeline.predict(X_test)
+        return compute_rmse(y_pred, y_test)
 
 
 if __name__ == "__main__":
